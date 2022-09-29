c7e106fd857f10cfd2f7a2ed040e0dcda0223d7f
Hazlina Kula <21B2122@ubd.edu.bn>
2022-09-30 01:20:54 +0800

    Update solutions.m

--- solutions.m
+++ solutions.m
@@ -1,4 +1,4 @@
-%Conway's Game of Life (Brute Force Method)
+% Conway's Game of Life (Brute Force Method)
 
 % size of the game, i.e  30,50,100, etc.
 n = 64;
@@ -7,19 +7,19 @@ n = 64;
 grid = round(rand(n,n));
 
 % number of generations
-gen=200;
+gen = 200;
 for m = 1:gen
 
 % convert to sparse matrix to save memory
 grid = sparse(grid);
 
-%clear borders
-grid(1,:)=0;
-grid(n,:)=0;
+% clear borders
+grid(1,:) = 0;
+grid(n,:) = 0;
 grid(:,1) = 0;
 grid(:,n) = 0;
 
-%create sum and new matrices
+% create sum and new matrices
 sum = zeros(n,n);
 new = zeros(n,n);
 
@@ -34,15 +34,15 @@ end
 for i = 2:n-1
   for j = 2:n-1
     if grid(i,j) == 1
-      if sum(i,j) == 2 % any live cells with two or three live neighbours lives on to the next generation
+      if sum(i,j) == 2           % any live cells with two or three live neighbours lives on to the next generation
         new(i,j) = 1;
-      elseif sum(i,j)==3
+      elseif sum(i,j) == 3
         new(i,j) = 1;
-      else
+      else                       % any live cells with fewer than two or more than three live neighbours dies
         new(i,j) = 0;
       end
     else
-      if sum(i,j) == 3
+      if sum(i,j) == 3           % any dead cells with exactly three live neighbours becomes a live cell (reproduction)
         new(i,j) = 1;
       else
         new(i,j) = 0;
